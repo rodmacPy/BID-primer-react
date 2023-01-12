439a2b25eeed976dc335e9d5eb0e114a3c4427c8
Enmanuel Rodriguez <enmanuelrodmac@gmail.com>
2023-01-12 18:53:58 -0300

    Entrega de tarea de react

---
+++ src/componentes/ListaTareas.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class ListaTareas extends Component {
+    render() {
+        return (
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the Dogs</li>
+            </ul>
+        );
+    }
+}
+export default ListaTareas;
---
+++ src/componentes/Subtitulo.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class Subtitulo extends Component {
+    render() {
+        return (
+            <h3>Things I need to do:</h3>
+        );
+    }
+}
+export default Subtitulo;
